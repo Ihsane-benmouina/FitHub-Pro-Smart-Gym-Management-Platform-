c439428056880cbea03ee8fef9bb479dcb61313e
ihsane ben-mouina <nom.prenom@univ-lille1.fr>
2026-09-11 12:31:05 +0100

    feat(auth): add user role field

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -10,7 +10,16 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password'])]
+#[Fillable([ 'name',
+    'email',
+    'password',
+    'phone',
+    'photo',
+    'birth_date',
+    'gender',
+    'address',
+    'is_active',
+    'role',])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
